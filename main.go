76730cb0efd6184d9c5630c27483c7dd898d41c3
FIzore <109813823+FIzore@users.noreply.github.com>
2024-09-25 19:38:43 +0800

    init

---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+import "gin-blog/routes"
+func main(){
+	routes.InitRouter()
+}
---
+++ routes/router.go
@@ -0,0 +1,20 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+
+
+
+func InitRouter(){
+	gin.SetMode(utils.AppMode)
+	r := gin.Default()
+
+	router = r.Group("/api/v1"){
+		router.GET("/hello", func(c *gin.Context){
+			c.JSON(200, gin.H{
+				"message": "Hello Gin",
+			})
+		}
+	}
+	r.Run(utils.HttpPort)
+}
