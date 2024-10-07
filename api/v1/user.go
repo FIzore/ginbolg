569928aa213a2413205af3e632e75ead6740dbe8
FIzore <109813823+FIzore@users.noreply.github.com>
2024-10-07 15:11:41 +0800

    构建路由模块

--- api/v1/user.go
+++ api/v1/user.go
@@ -1,33 +1,65 @@
 package v1
 
 import (
+	"ginblog/model"
+	"ginblog/utils/errmsg"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
-// 查询用户是否存在
+var code int
+
+// UserExist 查询用户是否存在
 func UserExist(c *gin.Context) {
-	//
 
 }
 
-// 添加用户
+// AddUser 添加用户
 func AddUser(c *gin.Context) {
-	//
+	var data model.User
+	_ = c.ShouldBindJSON(&data)
+	code = model.CheckUser(data.Username)
+	if code == errmsg.SUCCESS {
+		model.CreateUser(&data)
+	}
+	if code == errmsg.ERROR_USERNAME_USED {
+		code = errmsg.ERROR_USERNAME_USED
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 //查询单个用户
 
-// 查询用户列表
+// GetUsers 查询用户列表
 func GetUsers(c *gin.Context) {
-	//
+	pageSize, _ := strconv.Atoi(c.Query("pageSize"))
+	pageNum, _ := strconv.Atoi(c.Query("pageNum"))
+	if pageSize == 0 {
+		pageSize = -1 //查询所有数据,取消分页限制
+	}
+	if pageNum == 0 {
+		pageNum = -1 //取消分页限制
+	}
+	data := model.GetUsers(pageSize, pageNum)
+	code = errmsg.SUCCESS
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
-// 编辑用户
+// EditUser 编辑用户
 func EditUser(c *gin.Context) {
 	//
 }
 
-// 删除用户
+// DeleteUser 删除用户
 func DeleteUser(c *gin.Context) {
 	//
 }
--- model/User.go
+++ model/User.go
@@ -1,11 +1,51 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"ginblog/utils/errmsg"
+	"gorm.io/gorm"
+	"log"
+)
 
 type User struct {
 	gorm.Model
 	Username string `gorm:"type:varchar(20);not null" json:"username"`
 	Password string `gorm:"type:varchar(20);not null" json:"password"`
 	Role     int    `gorm:"type:int" json:"role"` //是否管理员
+}
+
+// CreateUser 新增用户
+func CreateUser(data *User) int {
+	err := db.Create(&data).Error
+	if err != nil {
+		return errmsg.ERROR //500
+	}
+	return errmsg.SUCCESS //200
 
 }
+
+// CheckUser 查询用户是否存在
+func CheckUser(name string) int {
+	var users User
+	db.Select("id").Where("username = ?", name).First(&users)
+	if users.ID > 0 {
+		return errmsg.ERROR_USERNAME_USED //1001
+	}
+
+	return errmsg.SUCCESS //200
+}
+
+// GetUsers 查询用户列表
+func GetUsers(pageSize int, pageNum int) []User {
+	var users []User
+	err := db.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&users).Error //分页查询
+	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+		log.Printf("Error fetching users: %v", err)
+		return []User{} // 返回空的切片而不是 nil
+	}
+	return users
+}
+
+// 编辑用户
+
+// DeleteUser 删除用户
