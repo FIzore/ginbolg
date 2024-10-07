22dcf0e06ae85b1db14d6cdbe04f71c5ac03edde
FIzore <109813823+FIzore@users.noreply.github.com>
2024-10-07 20:56:26 +0800

    增加加密功能

--- model/User.go
+++ model/User.go
@@ -1,8 +1,10 @@
 package model
 
 import (
+	"encoding/base64"
 	"errors"
 	"ginblog/utils/errmsg"
+	"golang.org/x/crypto/scrypt"
 	"gorm.io/gorm"
 	"log"
 )
@@ -16,6 +18,7 @@ type User struct {
 
 // CreateUser 新增用户
 func CreateUser(data *User) int {
+	data.Password = ScryptPw(data.Password) //对密码进行加密
 	err := db.Create(&data).Error
 	if err != nil {
 		return errmsg.ERROR //500
@@ -49,3 +52,16 @@ func GetUsers(pageSize int, pageNum int) []User {
 // 编辑用户
 
 // DeleteUser 删除用户
+
+// ScryptPw 密码加密
+func ScryptPw(password string) string {
+	const KeyLen = 10
+	salt := make([]byte, 8)
+	salt = []byte{12, 32, 4, 6, 66, 22, 222, 11}
+	HashPw, err := scrypt.Key([]byte(password), salt, 16384, 8, 1, KeyLen)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fpw := base64.StdEncoding.EncodeToString(HashPw)
+	return fpw
+}
